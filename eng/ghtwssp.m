3254cda975c39510e430fe966e2dad5df9335d36
Norton Allen <allen@huarp.harvard.edu>
2014-07-09 14:33:51 +0000

    Reorganization of plots

--- eng/ghtwssp.m
+++ eng/ghtwssp.m
@@ -1,5 +1,5 @@
 function fig = ghtwssp(varargin);
 % ghtwssp(...)
 % SSP
-ffig = ne_group(varargin,'SSP','phtwsspn','phtwsspsn','phtwssps','phtwsspstatus','phtwsspts','phtwsspt','phtwsspovf');
+ffig = ne_group(varargin,'SSP','phtwsspn','phtwsspsn','phtwsspts','phtwsspw','phtwssps','phtwsspovf');
 if nargout > 0 fig = ffig; end
