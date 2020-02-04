120677418cfb48ef73550105eaa73c13c2cfb892
Norton Allen <allen@huarp.harvard.edu>
2020-02-04 11:43:00 -0500

    Updated status for Exhaust, Gas commands

--- eng/phtwps.m
+++ eng/phtwps.m
@@ -2,4 +2,4 @@ function phtwps(varargin);
 % phtwps( [...] );
 % Pump Status
 h = ne_dstat({
-  'Scroll_Pump_DS', 'DS10', 4 }, 'Status', varargin{:} );
+  'Scroll_Pump_DS', 'DS08', 4 }, 'Status', varargin{:} );
