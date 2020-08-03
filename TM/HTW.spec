4709b8b73731fcdf2daa9860b29a05645f62faad
Norton Allen <allen@huarp.harvard.edu>
2020-08-03 15:10:56 -0400

    First pass throttle algo

--- TM/HTW.spec
+++ TM/HTW.spec
@@ -46,7 +46,7 @@ TGTDIR = $(TGTNODE)/home/HTW
 
 HTWcol : -lsubbus
 HTWsrvr : -lsubbus mains.oui
-HTWalgo : ptrh_conv.tmc idxflag.tmc dstat.tmc HTW.tma TV.tma PV.tma
+HTWalgo : ptrh_conv.tmc idxflag.tmc dstat.tmc HTW.tma TV.tma PV.tma Throttle.tma
 hkdisp : dstat.tmc QCLI_conv.tmc ptrh_conv.tmc hk.tbl
 gddisp : dstat.tmc idxflag.tmc mains_conv.tmc gas.tbl
 datadisp : dstat.tmc QCLI_conv.tmc SSPrtg.tmc data.tbl /usr/local/share/oui/cic.oui
--- TM/swstat.cmd
+++ TM/swstat.cmd
@@ -15,12 +15,20 @@
   : both { $0 = 3; }
   ;
 
-&gain <unsigned short>
+&gain <uint16_t>
   : %lf ( xx.x ) {
       double val = $1*10 + .5;
       if ( val < 0 ) val = 0.;
       else if ( val > 9999. ) val = 9999.;
-      $0 = (unsigned short)val;
+      $0 = (uint16_t)val;
     }
   ;
 
+&tvgain <int16_t>
+  : %lf ( [-]xx.x ) {
+      double val = $1*10 + .5;
+      if ( val < -30000. ) val = -30000.;
+      else if ( val > 30000. ) val = 30000.;
+      $0 = (int16_t)val;
+    }
+  ;
