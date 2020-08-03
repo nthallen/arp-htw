d5be1bc97f8500dac1ffb6b8e30f05824e38094c
Norton Allen <allen@huarp.harvard.edu>
2020-08-03 16:06:44 -0400

    genui outputs updated

--- eng/ghtwi.m
+++ eng/ghtwi.m
@@ -1,5 +1,5 @@
 function fig = ghtwi(varargin);
 % ghtwi(...)
 % Inlet
-ffig = ne_group(varargin,'Inlet','phtwidp','phtwit');
+ffig = ne_group(varargin,'Inlet','phtwidp','phtwit','phtwig');
 if nargout > 0 fig = ffig; end
