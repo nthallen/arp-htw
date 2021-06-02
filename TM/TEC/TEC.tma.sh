ab7d9cd3670cd7e25977f84b09e34c035e011a32
Norton Allen <allen@huarp.harvard.edu>
2021-06-02 10:21:38 -0400

    Ready to test

--- TM/TEC/TEC.tma.sh
+++ TM/TEC/TEC.tma.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#! /bin/sh
 # Gen TEC TMA
 
 cat <<EOF >TEC.tma
@@ -8,9 +8,9 @@ cat <<EOF >TEC.tma
 EOF
 
 sed -e 's/@PARTNO@/1/; s/@CHANNEL@/LTE/g; s/@DESC@/Laser TEC/; s/@IDLE@/\nState TEC_LTE_Idle {}\n/' TEC0.tma >>TEC.tma
-sed -e 's/@PARTNO@/2/; s/@CHANNEL@/LPV/g; s/@DESC@/Laser Pressure Vessel TEC/; s/@IDLE@//' TEC0.tma >>TEC.tma
+sed -e 's/@PARTNO@/2/; s/@CHANNEL@/LPV/g; s/@DESC@/LPV TEC/; s/@IDLE@//' TEC0.tma >>TEC.tma
 sed -e 's/@PARTNO@/3/; s/@CHANNEL@/Plate/g; s/@DESC@/Plate TEC/; s/@IDLE@//' TEC0.tma >>TEC.tma
 sed -e 's/@PARTNO@/4/; s/@CHANNEL@/Cover/g; s/@DESC@/Cover TEC/; s/@IDLE@//' TEC0.tma >>TEC.tma
-sed -e 's/@PARTNO@/5/; s/@CHANNEL@/Cell/g; s/@DESC@/Cell TEC/; s/@IDLE@//' TEC0.tma >>TEC.tma
-sed -e 's/@PARTNO@/6/; s/@CHANNEL@/DPV/g; s/@DESC@/Detector Pressure Vessel TEC/; s/@IDLE@//' TEC0.tma >>TEC.tma
+sed -e 's/@PARTNO@/5/; s/@CHANNEL@/Cell/g; s/@DESC@/Cell Temp/; s/@IDLE@//' TEC0.tma >>TEC.tma
+sed -e 's/@PARTNO@/6/; s/@CHANNEL@/DPV/g; s/@DESC@/DPV TEC/; s/@IDLE@//' TEC0.tma >>TEC.tma
 sed -e 's/@PARTNO@/7/; s/@CHANNEL@/BubC/g; s/@DESC@/Bubbler TEC/; s/@IDLE@//' TEC0.tma >>TEC.tma
