88ab0009a52e125432339b5a0926004e86aadc8e
Norton Allen <allen@huarp.harvard.edu>
2021-06-12 18:56:56 -0400

    Changes compiled

---
+++ eng/ghtwiwg1_stat.m
@@ -0,0 +1,5 @@
+function fig = ghtwiwg1_stat(varargin);
+% ghtwiwg1_stat(...)
+% IWG1 Stat
+ffig = ne_group(varargin,'IWG1 Stat','phtwiwg1_statcp','phtwiwg1_stattd','phtwiwg1_stats');
+if nargout > 0 fig = ffig; end
